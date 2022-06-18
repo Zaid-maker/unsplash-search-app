31641e1e4e88ecb19caa1cdf0545b911e7b07ebc
Zaid-maker <pzhafeez@gmail.com>
2022-06-18 16:41:28 +0500

    made the search works

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,65 @@
-<script></script>
+<script>
+  import Search from "./Search.svelte";
+  import SearchResults from "./SearchResults.svelte";
+
+  const UNSPLASH_ACCESS_KEY = "EJ49yYYs-cEuoMtG4Q9dtmWEOyp3A4nJAzUaQV1qqTA";
+
+  let searchQuery = "";
+  let searchTerm = null;
+  let totalPages = null;
+  let searchResults = [];
+  let nextPage = 1;
+  let isLoading = false;
+
+  function handleSubmit() {
+    searchTerm = searchQuery.trim();
+    searchResults = [];
+    totalPages = null;
+    nextPage = 1;
+
+    if (!searchTerm) return;
+
+    searchUnsplash();
+  }
+
+  function searchUnsplash() {
+    isLoading = true;
+
+    const endpoint = `https://api.unsplash.com/search/photos?query=${searchTerm}&page=${nextPage}&per_page=28&client_id=${UNSPLASH_ACCESS_KEY}`;
+
+    fetch(endpoint)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok.");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        if (data.total === 0) {
+          alert("No photoes were found for your search query.");
+          return;
+        }
+
+        searchResults = [...searchResults, ...data.results];
+        totalPages = data.total_pages;
+
+        if (nextPage < totalPages) {
+          nextPage += 1;
+        }
+      })
+      .catch(() => alert("Something went wrong."))
+      .finally(() => {
+        isLoading = false;
+      });
+  }
+</script>
 
 <main class="App">
   <h1>Unsplash Search App</h1>
+
+  <Search bind:query={searchQuery} {handleSubmit} />
+
+  <SearchResults results={searchResults} />
 </main>
 
 <style>
---
+++ src/search.svelte
@@ -0,0 +1,27 @@
+<script>
+  export let query;
+  export let handleSubmit;
+</script>
+
+<div class="search">
+  <form class="search-form" on:submit|preventDefault={handleSubmit}>
+    <input
+      bind:value={query}
+      type="search"
+      class="search-input"
+      placeholder="Search Unsplash's library over 1 million photos"
+    />
+  </form>
+</div>
+
+<style>
+  .search-input {
+    width: 100%;
+    max-width: 800px;
+    border-radius: 4px;
+    border: 1px solid #ccc;
+    padding: 10px 20px;
+    font-size: 20px;
+    margin-bottom: 50px;
+  }
+</style>
